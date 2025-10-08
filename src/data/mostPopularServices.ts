a886f30123d55e41872272eed08fda0983b3d55e
Amir11f <zohrabiamirhossain@gmail.com>
2025-10-08 10:19:25 +0330

    feat:most popular services component created with initial design

---
+++ src/data/mostPopularServices.ts
@@ -0,0 +1,26 @@
+
+interface Services {
+    image:string,
+    service:string
+    id:number
+}
+
+export const popularServices:Services[] = [
+    {
+        image:'/images/securityCamera.jpg',
+        service:'نصب دوربین',
+        id:1
+    },    {
+        image:'/images/securityCamera.jpg',
+        service:'نصب دوربین',
+        id:2
+    },    {
+        image:'/images/securityCamera.jpg',
+        service:'نصب دوربین',
+        id:3
+    },    {
+        image:'/images/securityCamera.jpg',
+        service:'نصب دوربین',
+        id:4
+    },
+]
