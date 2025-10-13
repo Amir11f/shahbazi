20c0f086f8c8a6d84405448989490a67e6ee3a15
Amir11f <zohrabiamirhossain@gmail.com>
2025-10-13 12:29:38 +0330

    feat:InstagramLikeVideos component added

---
+++ src/data/instagramLikeVideos.ts
@@ -0,0 +1,43 @@
+
+
+export interface InstagramLike {
+    projectId:number,
+    videoUrl:string
+}
+
+export const instagremLike:InstagramLike[] = [
+    {
+        projectId:1,
+        videoUrl:''
+    },    {
+        projectId:1,
+        videoUrl:''
+    },    {
+        projectId:12,
+        videoUrl:''
+    },    {
+        projectId:14,
+        videoUrl:''
+    },    {
+        projectId:31,
+        videoUrl:''
+    },{
+        projectId:313,
+        videoUrl:''
+    },{
+        projectId:3341,
+        videoUrl:''
+    },{
+        projectId:3321,
+        videoUrl:''
+    },{
+        projectId:351,
+        videoUrl:''
+    },{
+        projectId:33451,
+        videoUrl:''
+    },{
+        projectId:35531,
+        videoUrl:''
+    },
+]
