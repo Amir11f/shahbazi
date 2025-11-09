6a2cd32b10fab5b190312558b257661688e1d767
Amir11f <zohrabiamirhossain@gmail.com>
2025-11-09 12:25:55 +0330

    chore: RoadMap component creatd

---
+++ src/components/RoadMap.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+export default function RoadMap() {
+  return (
+    <div className="font-cursive flex flex-row-reverse items-center justify-around max-xl:hidden">
+      <div className="flex flex-col items-end">
+        <p className="text-[54px] font-bold w-[530px]">
+          متن برای راحتی درک کارت های روبه رو و مفهوم رود مپ
+        </p>
+        <p className="w-103 text-lg font-thin">
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+          از طراحان گرافیک است چاپگرها
+        </p>
+      </div>
+      <div className="flex flex-col gap-[14px] ml-20">
+        <div className="flex flex-row items-center">
+          <div className="w-[513px] h-[142px] rounded-[18px] bg-black flex flex-row-reverse text-white justify-between">
+            <p className="text-xl flex items-center justify-center w-[146px] mr-15">
+              توضیحات کارت در این قسمت
+            </p>
+            <hr className="w-[95px] h-[1px] rotate-90 relative top-18 right-3 bg-[#d5d5d5]" />
+            <p className="text-sm font-thin flex items-center justify-center w-[233px] ml-8 text-[#d5d5d5]">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان گرافیک است چاپگرها
+            </p>
+          </div>
+          <div className="w-13 h-13 bg-[#F1F1F1] rounded-full relative right-6.5 flex items-center justify-center text-2xl">
+            01
+          </div>
+        </div>
+        <div className="flex flex-row items-center">
+          <div className="w-[513px] h-[142px] rounded-[18px] bg-[#565656] flex flex-row-reverse text-white justify-between">
+            <p className="text-xl flex items-center justify-center w-[146px] mr-15">
+              توضیحات کارت در این قسمت
+            </p>
+            <hr className="w-[95px] h-[1px] rotate-90 relative top-18 right-3 bg-[#d5d5d5]" />
+            <p className="text-sm font-thin flex items-center justify-center w-[233px] ml-8 text-[#d5d5d5]">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان گرافیک است چاپگرها
+            </p>
+          </div>
+          <div className="w-13 h-13 bg-[#F1F1F1] rounded-full relative right-6.5 flex items-center justify-center text-2xl">
+            02
+          </div>
+        </div>{" "}
+        <div className="flex flex-row items-center">
+          <div className="w-[513px] h-[142px] rounded-[18px] bg-[#BCBCBC] flex flex-row-reverse text-black justify-between">
+            <p className="text-xl flex items-center justify-center w-[146px] mr-15">
+              توضیحات کارت در این قسمت
+            </p>
+            <hr className="w-[95px] h-[1px] rotate-90 relative top-18 right-3 bg-[#d5d5d5]" />
+            <p className="text-sm font-thin flex items-center justify-center w-[233px] ml-8 text-black">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان گرافیک است چاپگرها
+            </p>
+          </div>
+          <div className="w-13 h-13 bg-[#F1F1F1] rounded-full relative right-6.5 flex items-center justify-center text-2xl">
+            03
+          </div>
+        </div>{" "}
+        <div className="flex flex-row items-center">
+          <div className="w-[513px] h-[142px] rounded-[18px] bg-[#EEEEEE] flex flex-row-reverse text-black justify-between">
+            <p className="text-xl flex items-center justify-center w-[146px] mr-15">
+              توضیحات کارت در این قسمت
+            </p>
+            <hr className="w-[95px] h-[1px] rotate-90 relative top-18 right-3 bg-[#d5d5d5]" />
+            <p className="text-sm font-thin flex items-center justify-center w-[233px] ml-8 text-black">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان گرافیک است چاپگرها
+            </p>
+          </div>
+          <div className="w-13 h-13 bg-[#F1F1F1] rounded-full relative right-6.5 flex items-center justify-center text-2xl">
+            04
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
