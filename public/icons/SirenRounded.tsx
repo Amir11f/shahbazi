812fa3000ac8caa5a42e2f0b55af475cb7697a48
Amir11f <zohrabiamirhossain@gmail.com>
2025-11-13 15:27:34 +0330

    chore:adding some react component icons

---
+++ public/icons/SirenRounded.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+
+const NotificationIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width="77"
+    height="77"
+    viewBox="0 0 77 77"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M64.1666 70.5834V51.3334C64.1666 37.158 52.6753 25.6667 38.4999 25.6667C24.3246 25.6667 12.8333 37.158 12.8333 51.3334V70.5834"
+      stroke="#C9A35B"
+      strokeWidth="3"
+    />
+    <path
+      d="M45.8486 36.8958C49.0656 38.198 51.6345 40.7666 52.9368 43.9836"
+      stroke="#C9A35B"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+    <path
+      d="M6.41675 70.5833H70.5834"
+      stroke="#C9A35B"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+    <path
+      d="M38.5 6.41675V16.0417"
+      stroke="#C9A35B"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+    <path
+      d="M67.375 19.25L62.5625 24.0625"
+      stroke="#C9A35B"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+    <path
+      d="M9.625 19.25L14.4375 24.0625"
+      stroke="#C9A35B"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+    <path
+      d="M43.3125 56.1458C43.3125 58.8035 41.1578 60.9583 38.5 60.9583C35.8422 60.9583 33.6875 58.8035 33.6875 56.1458C33.6875 53.488 35.8422 51.3333 38.5 51.3333C41.1578 51.3333 43.3125 53.488 43.3125 56.1458Z"
+      stroke="#C9A35B"
+      strokeWidth="3"
+    />
+    <path
+      d="M38.5 60.9583V70.5833"
+      stroke="#C9A35B"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+  </svg>
+);
+
+export default NotificationIcon;
